61bf7dfc41b8e91d3640a04d7749ab194318ffd0
Bekarys <bsailaukul@gmail.com>
2019-01-31 22:47:31 +0600

    Fourh task

---
+++ week1/Task4/Task4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            a = int.Parse(Console.ReadLine()); 
+            string[,] arr = new string[100,100];
+            for (int i = 0; i<a; i++)
+            {
+                for(int j=0; j<=i; j++)
+                {
+                    arr[i, j] = "[*]";
+                }
+            }
+            for (int i = 0; i <= a; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write(arr[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
