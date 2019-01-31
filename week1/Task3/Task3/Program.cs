3df421f99ed4cb8b34d4c7cd1a5fd0081f854a5f
Bekarys <bsailaukul@gmail.com>
2019-01-31 21:03:26 +0600

    Third task

---
+++ week1/Task3/Task3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+
+        public static int [] rep(int []  b)
+        {
+            int[] newar = new int[b.Length * 2];
+            for(int i=0, j=0; i<newar.Length; i++, j++)
+            {
+                newar[i] = b[j];
+                i++;
+                newar[i] = b[j];
+                
+            }
+            return newar;
+        }
+        static void Main(string[] args)
+        {
+            int a;
+            a = int.Parse(Console.ReadLine());
+            int[] arr = new int[a];
+            string s = Console.ReadLine();
+            string[] mas = s.Split();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = int.Parse(mas[i]);
+            }
+            int[] newar = rep(arr);
+            for (int i = 0; i < newar.Length; i++)
+            {
+                Console.Write(newar[i] + " ");
+            }
+            Console.ReadKey(); 
+        } 
+    }
+}
