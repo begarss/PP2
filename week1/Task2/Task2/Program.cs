62d0c66dc770b1b9a5d581a053713506257f47be
Bekarys <bsailaukul@gmail.com>
2019-01-31 19:32:39 +0600

    Second task

---
+++ week1/Task2/Task2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Student
+    {
+
+        public string name;
+        public int id;
+        public int year=0;
+
+        public Student(string _name, int _id)
+        {
+            name = _name;
+            id = _id;
+            year = year + 1;
+            
+        }
+        public void Print()
+        {
+            Console.WriteLine("Name " + name);
+            Console.WriteLine("ID " + id);
+            Console.WriteLine("Year of study " + year);
+            Console.ReadKey();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student Newstudent = new Student("Bekarys", 001);
+            Newstudent.Print();
+           
+        }
+    }
+}
