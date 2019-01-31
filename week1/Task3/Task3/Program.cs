5c39b5d943569cd4f1bdbeaf9af4676d3d006b57
Bekarys <bsailaukul@gmail.com>
2019-01-31 21:17:39 +0600

    Comments are adde

--- week1/Task3/Task3/Program.cs
+++ week1/Task3/Task3/Program.cs
@@ -9,33 +9,33 @@ namespace Task3
     class Program
     {
 
-        public static int [] rep(int []  b)
+        public static int [] rep(int []  b)// создаем метод для изменение массива
         {
-            int[] newar = new int[b.Length * 2];
+            int[] newar = new int[b.Length * 2];// создаем новый массив с размерностью в два раза больше
             for(int i=0, j=0; i<newar.Length; i++, j++)
             {
-                newar[i] = b[j];
-                i++;
+                newar[i] = b[j];// внесение элемента массива в новый
+                i++;// увеливаем индекс нового массива чтобы внесте этот же массив в новый массив второй раз
                 newar[i] = b[j];
                 
             }
-            return newar;
+            return newar;// возвращаем новый массив
         }
         static void Main(string[] args)
         {
             int a;
-            a = int.Parse(Console.ReadLine());
-            int[] arr = new int[a];
-            string s = Console.ReadLine();
-            string[] mas = s.Split();
+            a = int.Parse(Console.ReadLine());// вводим колво элементов
+            int[] arr = new int[a];// массив с размерностью введеного ранее числа
+            string s = Console.ReadLine();// вводим наши элементы в линию
+            string[] mas = s.Split();// разделяем элементы по пробелу
             for (int i = 0; i < arr.Length; i++)
             {
-                arr[i] = int.Parse(mas[i]);
+                arr[i] = int.Parse(mas[i]);// переносим разделенные элементы в массив инт
             }
-            int[] newar = rep(arr);
+            int[] newar = rep(arr);// создаем новый массив и вызываем метод, отправляем в метод наш старый массив
             for (int i = 0; i < newar.Length; i++)
             {
-                Console.Write(newar[i] + " ");
+                Console.Write(newar[i] + " ");// выводим новый массив
             }
             Console.ReadKey(); 
         } 
