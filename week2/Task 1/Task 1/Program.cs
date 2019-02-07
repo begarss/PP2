35dcdde2ba1f2b40c47635e8615db311768646ba
Bekarys <bsailaukul@gmail.com>
2019-02-07 19:00:32 +0600

    lab2

---
+++ week2/Task 1/Task 1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text;
+
+namespace Task_1
+{
+    class Program
+    {
+        public static bool poli(string s)
+        {
+            int i = 0;
+            int j = s.Length - 1;
+
+            while (i < j)
+            {
+                if (s[i] != s[j])
+                {
+                    return false;
+                }
+                i++;  
+                    j--;
+            }
+            return true;
+        }
+       
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader("input.txt");
+            String s = sr.ReadToEnd();
+            if (poli(s))
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+                Console.WriteLine("No");
+            Console.ReadKey();
+        }
+    }
+}
