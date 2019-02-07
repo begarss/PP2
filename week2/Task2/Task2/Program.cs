ef7c1a22e1e3b8d9f35908ac3f1ebd7085290251
Bekarys <bsailaukul@gmail.com>
2019-02-07 19:27:27 +0600

    task2

---
+++ week2/Task2/Task2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task2
+{
+    class Program
+    {
+        public static bool Isprime(int n)
+        {
+            if (n == 1)
+            {
+                return false;
+            }
+            if (n == 2)
+            {
+                return true;
+            }
+            for (int i = 2; i <= Math.Sqrt(n); i++)
+            {
+                if (n % i == 0)
+                {
+                    return false;
+
+                }
+            }
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader("input.txt");
+            string s = sr.ReadLine();
+            string[] ar = s.Split();
+            int[] arr = new int[ar.Length];
+            for (int i = 0; i < ar.Length; i++)
+            {
+                arr[i] = int.Parse(ar[i]);
+            }
+
+            sr.Close();
+                    StreamWriter sw = new StreamWriter("output.txt");
+            for (int i = 0; i < arr.Length; i++)
+            {
+
+                if (Isprime(arr[i]))
+                {
+                    sw.Write(arr[i] + " ");
+                }
+
+            }
+            sw.Close();
+        }
+    }
+}
