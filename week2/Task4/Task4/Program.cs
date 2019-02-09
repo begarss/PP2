ea3276b3c4cdcc1c91096ad7935fcdc994d649a9
Bekarys <bsailaukul@gmail.com>
2019-02-09 17:06:48 +0600

    sool

---
+++ week2/Task4/Task4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+    
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path, path2;
+           // Console.WriteLine("Введите путь к файлу и имя");
+            path = Console.ReadLine();
+            Console.WriteLine("Введите путь куда вы будете копировать и имя файла");
+            path2 = Console.ReadLine();
+            FileInfo file = new FileInfo(path);
+            file.CopyTo(path2);
+            file.Delete();
+
+
+
+        
+        }
+    }
+}
